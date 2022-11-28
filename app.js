4637b39e8a73eb118429cd4544e33082d457602b
iskender <even10iq@gmail.com>
2022-11-28 21:40:26 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// task 1 Given an integer x, return true if x is a  palindrome , and false otherwise.
+
+// const func = (int) => {
+//   let str = int.toString();
+//   let newStr = str.split("").reverse().join("");
+//   if (str === newStr) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(func(12));
+
+//task 2 (not solved)Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by re  arranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// var twoSum = function (nums, target) {
+//     let arr = [];
+//     for (let i = 0; i < nums.length; i++) {
+//       for (let j = i+1; j < nums.length; j++) {
+//         if (nums[i] + nums[j] === target) {
+//           arr.push(i, j);
+//         }
+//       }
+//     }
+//     return arr;
+// };
+// console.log(twoSum([2, 7, 11, 15], 9));
